adacc18db97cc4e3bbaa0c5888bf96d9c8ad2101
DJCEL <16272026+DJCEL@users.noreply.github.com>
2025-09-06 23:55:41 +0200

    update

---
+++ Translator/src/translator.py
@@ -0,0 +1,40 @@
+from openai import OpenAI
+import pandas as pd
+
+def translate_missing(language):
+    file_missing = f"./excel/Missing/Missing_{language}.xlsx"
+
+    df = pd.read_excel(file_missing)
+    if df.empty:
+      print("No missing translations found.")
+      return
+
+    tag = df.iloc[0]['Tag']
+    print("Tag =", tag)
+
+    input_text_english = df.iloc[0]['English']
+    print("English =",input_text_english)
+  
+    client = OpenAI()
+
+    result = client.responses.create(
+        model="gpt-5",
+        input=f"Translate in {language} the following text: {input_text_english}",
+        reasoning={ "effort": "low" },
+        text={ "verbosity": "low" },
+    )
+
+    translation = result.output_text
+
+    print(f"{language} =", translation)
+    print("\n")
+
+    #df.iloc[0]['French'] = translation
+
+
+def main():
+    language = "French"
+    translate_missing(language)
+
+if __name__ == "__main__":
+   main()
