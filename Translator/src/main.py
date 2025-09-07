808372b260c3e8ed52989a4f295e4baffb46c78d
DJCEL <16272026+DJCEL@users.noreply.github.com>
2025-09-07 11:07:13 +0200

    update

--- Translator/src/main.py
+++ Translator/src/main.py
@@ -3,11 +3,11 @@ from translator import translate_missing
 
 def main():
     languages_list = ["English","French","German","Italian","Dutch","Spanish","Greek","Portuguese","Japanese","Russian","Chinese (simplified)","Arabic"]
-    languagetotranslate_list = ["French"]
+    languagestotranslate_list = ["French"]
 
     prepare_process_languages(languages_list)
 
-    for language in languagetotranslate_list:
+    for language in languagestotranslate_list:
         translate_missing(language)
 
 if __name__ == "__main__":
