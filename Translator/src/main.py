31adf5db14f0895858530efea4d4ef1a375f742a
DJCEL <16272026+DJCEL@users.noreply.github.com>
2025-09-07 11:47:50 +0200

    update

--- Translator/src/main.py
+++ Translator/src/main.py
@@ -4,10 +4,12 @@ from translator import translate_missing_list
 def main():
     languages_list = ["English","French","German","Italian","Dutch","Spanish","Greek","Portuguese","Japanese","Russian","Chinese (simplified)","Arabic"]
     languagestotranslate_list = ["French"]
+    bPrepare_step = True
 
     """ We prepare the missing translations by language (file compared to English.xml) """
     """ We use Excel files to store the data """
-    prepare_process_languages(languages_list)
+    if bPrepare_step:
+        prepare_process_languages(languages_list)
 
     """ We translate the missing translations with ChatGPT (gpt-5 model) """
     translate_missing_list(languagestotranslate_list)
